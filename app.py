8a698ed0b820f3ec93c43b6d2c73704096f8918f
abdomar <abdomarr.94@gmail.com>
2023-07-09 13:51:37 +0300

    python oop

--- app.py
+++ app.py
@@ -1,13 +1,74 @@
+from datetime import date
 class Student:
     nb_of_students=0
 
-    def __init__(abd,name:str,age,courses) -> None:
-        abd.name = name
-        abd.age=age
-        abd.courses=courses
+    def __init__(self,name:str,age,courses=[]):
+        self.__name = name ## __ for private attribute
+        self.__age=age
+        self.__courses=courses
         Student.nb_of_students+=1
 
+    ##getter and setter
+    def get_name(self):
+        return self.__name
+    def set_name(self,name):
+        self.__name=name
+
+    def get_age(self):
+        return self.__age
+    def set_age(self,age):
+        self.__age=age
+
+    def get_courses(self):
+        return self.__courses
+    def set_courses(self,courses):
+        self.__courses=courses
+
+    def describe(self):
+        print(f'my name is {self.__name}')
+    
+    def isOld(self,age1):
+        return self.__age>age1
+    
+    @classmethod # decorator class method 
+    #kerml multple constructor
+    def initFromBirthYear(cls,name,birthYear):
+        return cls(name,date.today().year-birthYear)
+
+
+
+
+class Pizza:
+    def __init__(self,ingredients) -> None:
+        self.ingredients=ingredients
+
+    @classmethod
+    def veg(cls):
+        return cls(["onion","mash"])
+    
+    ## magic functions
+    def __str__(self) -> str:
+        return f'Pizza ingredients are {self.ingredients}'
+
+    @staticmethod
+    def shiTayb():
+        print("ktiiiir taybi")
 
 s1=Student("abd",29,["math"])
 s1.age=24
-print(s1.age)
+s1.describe()
+print(s1.isOld(20))
+s1.set_name("omar")
+s1.name="testtt"
+print(s1.get_name())
+
+print(s1.get_age())
+
+s2=Student.initFromBirthYear("ahmad",1993)
+print(s2.get_name())
+print(type(s2))
+
+p1=Pizza.veg()
+print(p1.ingredients)
+print(p1)
+Pizza.shiTayb()
---
+++ inherit.py
@@ -0,0 +1,33 @@
+from datetime import date
+class Person:
+    def __init__(self,name,age) -> None:
+        self.name=name
+        self.age=age
+    
+    def display(self):
+        return "description:"+ self.name
+
+    @classmethod
+    def anotherConstructor(cls,name,birthYear,extra):
+        return cls(name,date.today().year-birthYear,extra)
+
+class Man(Person):
+    gender="Male"
+    nb_of_men=0
+
+    def __init__(self, name, age,voice) -> None:
+        super().__init__(name, age)
+        self.voice=voice
+        Man.nb_of_men+=1
+
+    def display(self):
+        s1= super().display()
+        return s1 + self.voice
+    
+m1=Man("abd",54,"hard")
+print(m1.display())
+print(Man.nb_of_men)
+
+m2=Man.anotherConstructor("m",2000,"soft")
+print(m2.voice)
+print(isinstance(m2,Person))
